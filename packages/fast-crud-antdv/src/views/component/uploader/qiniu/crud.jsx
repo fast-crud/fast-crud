df939e385c5acfc5c391c29a721216635cccb091
xiaojunnuo <xiaojunnuo@qq.com>
2021-03-16 23:22:31 +0800

    refactor: file-uploader

---
+++ packages/fast-crud-antdv/src/views/component/uploader/qiniu/crud.jsx
@@ -0,0 +1,73 @@
+import * as api from "./api";
+import { requestForMock } from "/src/api/service";
+import { dict } from "/src/fs";
+export default function ({ crudRef }) {
+  const pageRequest = async (query) => {
+    return await api.GetList(query);
+  };
+  const editRequest = async ({ form, row }) => {
+    form.id = row.id;
+    return await api.UpdateObj(form);
+  };
+  const delRequest = async (id) => {
+    return await api.DelObj(id);
+  };
+
+  const addRequest = async ({ form }) => {
+    return await api.AddObj(form);
+  };
+  return {
+    request: {
+      pageRequest,
+      addRequest,
+      editRequest,
+      delRequest,
+    },
+    columns: {
+      id: {
+        title: "ID",
+        key: "id",
+        type: "number",
+        column: {
+          width: 50,
+        },
+        form: {
+          show: false,
+        },
+      },
+      file: {
+        title: "七牛云上传",
+        type: "file-uploader",
+        form: {
+          component: {
+            uploader: {
+              type: "qiniu",
+            },
+          },
+        },
+      },
+      pictureCard: {
+        title: "照片墙",
+        type: "image-uploader",
+        form: {
+          component: {
+            uploader: {
+              type: "qiniu",
+            },
+          },
+        },
+      },
+      cropper: {
+        title: "裁剪",
+        type: "cropper-uploader",
+        form: {
+          component: {
+            uploader: {
+              type: "qiniu",
+            },
+          },
+        },
+      },
+    },
+  };
+}
