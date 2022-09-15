52c43976272a8f3bde27ec919225fc5a7c72d27c
xiaojunnuo <xiaojunnuo@qq.com>
2022-09-15 12:33:07 +0800

    refactor: 1

--- packages/fast-crud/src/components/container/fs-container.vue
+++ packages/fast-crud/src/components/container/fs-container.vue
@@ -106,7 +106,7 @@ export default defineComponent({
       }
       .body {
         flex: 1;
-        overflow: hidden;
+        overflow-y: hidden;
       }
       .footer {
         flex-shrink: 0;
