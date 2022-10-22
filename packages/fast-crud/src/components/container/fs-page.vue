72766a5d7c7306f18f57a86d0f84439522648b0c
xiaojunnuo <xiaojunnuo@qq.com>
2022-10-22 23:37:07 +0800

    feat: fs-page组件外层增加fs-page-container包裹，防止出现样式错乱

--- packages/fast-crud/src/components/container/fs-page.vue
+++ packages/fast-crud/src/components/container/fs-page.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="fs-page">
-    <div v-if="$slots.header != null" class="fs-page-header">
-      <slot name="header"></slot>
-    </div>
+  <div class="fs-page-container">
+    <div class="fs-page">
+      <div v-if="$slots.header != null" class="fs-page-header">
+        <slot name="header"></slot>
+      </div>
 
-    <div class="fs-page-content">
-      <slot></slot>
-    </div>
-    <div v-if="$slots.footer != null" class="fs-page-footer">
-      <slot name="footer"></slot>
+      <div class="fs-page-content">
+        <slot></slot>
+      </div>
+      <div v-if="$slots.footer != null" class="fs-page-footer">
+        <slot name="footer"></slot>
+      </div>
     </div>
   </div>
 </template>
@@ -20,34 +22,40 @@ export default {
 </script>
 
 <style lang="less">
-.fs-page {
-  width:100%;
-  top: 0;
-  bottom: 0;
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-
-  .fs-page-header {
+.fs-page-container {
+  width: 100%;
+  height: 100%;
+  min-height: 300px;
+  position: relative;
+  .fs-page {
+    width: 100%;
+    top: 0;
+    bottom: 0;
+    position: absolute;
     display: flex;
-    align-items: center;
-    border-bottom: 1px solid #ddd;
-    > .title {
+    flex-direction: column;
+
+    .fs-page-header {
+      display: flex;
+      align-items: center;
+      border-bottom: 1px solid #ddd;
+      > .title {
+        flex: 1;
+        justify-self: flex-start;
+        padding: 18px;
+        font-size: 16px;
+        font-weight: bold;
+        color: #444;
+      }
+      > .more {
+        justify-self: flex-end;
+        margin-right: 18px;
+      }
+    }
+    .fs-page-content {
       flex: 1;
-      justify-self: flex-start;
-      padding: 18px;
-      font-size: 16px;
-      font-weight: bold;
-      color: #444;
+      overflow-y: auto;
     }
-    > .more {
-      justify-self: flex-end;
-      margin-right: 18px;
-    }
-  }
-  .fs-page-content {
-    flex: 1;
-    overflow-y: auto;
   }
 }
 </style>
