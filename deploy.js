713d7f1e0afd1404ccdfa132ff88fc7975fd9977
xiaojunnuo <xiaojunnuo@qq.com>
2023-02-09 18:08:08 +0800

    refactor: deploy

---
+++ deploy.js
@@ -0,0 +1,31 @@
+const http = require("axios")
+const naive = "http://flow-openapi.aliyun.com/pipeline/webhook/Zm3TJyDtyFZgV4dtJiD1"
+const doc = "http://flow-openapi.aliyun.com/pipeline/webhook/soOYdQ5sF3kLjTPJGmIO"
+const antdv = "http://flow-openapi.aliyun.com/pipeline/webhook/HiL0uVYxfUnBzIMJZVXB"
+const element = "http://flow-openapi.aliyun.com/pipeline/webhook/uFTI0XJ9RgqnofX7jpRD"
+
+const webhooks = [doc,naive,antdv,element]
+
+async function sleep(){
+    return new Promise(resolve => {
+        setTimeout(resolve,1000)
+    })
+}
+
+async function trigger(){
+    for (const webhook of webhooks) {
+        await http({
+            url:webhook,
+            method:'POST',
+            headers:{
+                "Content-Type": "application/json"
+            },
+            data:{}
+        })
+        console.log(`webhook success:${webhook}`)
+        await sleep()
+    }
+
+}
+
+trigger()
