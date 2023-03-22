c2f68d8afff5f0bd961cb7c02ab9d2fe4630e5a8
xiaojunnuo <xiaojunnuo@qq.com>
2023-03-23 00:41:03 +0800

    refactor: ui interface

--- packages/ui/ui-interface/src/context.ts
+++ packages/ui/ui-interface/src/context.ts
@@ -9,9 +9,7 @@ export class UiContext {
 
   get(): UiInterface {
     if (this.ref.value == null) {
-      throw new Error(
-        "您还未设置ui,先安装依赖@fast-crud/ui-interface,然后在use(FastCrud)前安装ui，安装代码：app.use(UiXxx)"
-      );
+      throw new Error("您还未设置ui,先安装依赖@fast-crud/ui-interface,然后在use(FastCrud)前安装ui，app.use(UiXxx)");
     }
     return this.ref.value;
   }
