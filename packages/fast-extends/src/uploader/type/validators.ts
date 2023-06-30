f6a8103a9329b20f4ab6cbbe52f2bd14cf5a9c90
xiaojunnuo <xiaojunnuo@qq.com>
2023-06-30 10:02:34 +0800

    chore:

--- packages/fast-extends/src/uploader/type/validators.ts
+++ packages/fast-extends/src/uploader/type/validators.ts
@@ -1,4 +1,4 @@
-import { compute } from "@fast-crud/fast-crud";
+import { compute, RuleRecord } from "@fast-crud/fast-crud";
 export const createAllUploadSuccessValidator = (getFormComponentRef: (key: string, isAsync: boolean) => any) => {
   return async (rule: any, value: any) => {
     const ref = await getFormComponentRef(rule.fullField, true);
@@ -13,4 +13,17 @@ export const AllUploadSuccessValidator = () => {
   return compute(({ getComponentRef }) => {
     return createAllUploadSuccessValidator(getComponentRef);
   });
+};
+
+export const createUploaderRules = (yourRules?: RuleRecord[], uploadingWarningMessage?: string): RuleRecord[] => {
+  if (yourRules == null) {
+    yourRules = [];
+  }
+  yourRules.push({
+    //@ts-ignore
+    validator: AllUploadSuccessValidator(), //如果要自定义校验规则则需要手动配置这个
+    message: uploadingWarningMessage || "还有文件正在上传，请稍候",
+    trigger: "blur" // <-------注意使用blur事件，否则会闪现
+  });
+  return yourRules;
 };
