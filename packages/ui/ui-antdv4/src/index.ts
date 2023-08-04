8066bf47a954650d812b8912aecb7fa91530149c
xiaojunnuo <xiaojunnuo@qq.com>
2023-08-04 18:22:12 +0800

    chore: antdv4 start

---
+++ packages/ui/ui-antdv4/src/index.ts
@@ -0,0 +1,25 @@
+import { message, notification, Modal } from "ant-design-vue";
+import { uiContext } from "@fast-crud/ui-interface";
+import setupIcons from "./icons";
+import { Antdv } from "./antdv";
+export * from "./antdv";
+
+export type UiSetupOptions = {
+  setupIcons?: boolean;
+};
+
+export default {
+  install(app: any, options: UiSetupOptions = {}) {
+    const antdvUi = new Antdv({
+      Message: message,
+      Notification: notification,
+      MessageBox: Modal
+    });
+    uiContext.set(antdvUi);
+
+    if (options.setupIcons !== false) {
+      setupIcons(app);
+    }
+    return antdvUi;
+  }
+};
