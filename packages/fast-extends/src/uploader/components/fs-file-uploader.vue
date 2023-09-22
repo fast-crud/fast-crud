c0678c61f1459fd9945ee61fe9dc05e47419831c
xiaojunnuo <xiaojunnuo@qq.com>
2023-09-22 22:30:11 +0800

    chore:

--- packages/fast-extends/src/uploader/components/fs-file-uploader.vue
+++ packages/fast-extends/src/uploader/components/fs-file-uploader.vue
@@ -104,7 +104,8 @@ export default defineComponent({
     },
     /**
      * 返回值类型
-     * 支持：`[object,url,key,其他（successHandle返回的object内要有该字段）]`
+     * 支持：`[object,url,key,其他（successHandle返回的object内要有该字段，不要用'id'）]`
+     * 如果配置了非url，则需要配置buildUrl用于反显
      */
     valueType: {
       type: String, // url ,key, object
