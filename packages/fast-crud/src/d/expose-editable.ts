9e0beb06470b564d490ec9393afe978f8a66c2fd
xiaojunnuo <xiaojunnuo@qq.com>
2024-10-27 01:46:31 +0800

    perf: editable row 优化添加

--- packages/fast-crud/src/d/expose-editable.ts
+++ packages/fast-crud/src/d/expose-editable.ts
@@ -10,6 +10,7 @@ import { EditableOnEnabledProps } from "../use";
 export type EditableAddRowOptions<R = any> = {
   row?: R;
   active?: boolean;
+  addRowFunc?: ({ row: R }) => void;
 };
 
 export type EditableActiveColsOptions = {
