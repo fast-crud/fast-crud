599cd4e5411d711af3a305bd40331e5e7569119a
xiaojunnuo <xiaojunnuo@qq.com>
2024-11-26 18:08:35 +0800

    chore:

--- packages/fast-extends/vite.config.js
+++ packages/fast-extends/vite.config.js
@@ -5,7 +5,7 @@ import {visualizer} from "rollup-plugin-visualizer";
 import strip from "@rollup/plugin-strip";
 import DefineOptions from "unplugin-vue-define-options/vite";
 import typescript from "@rollup/plugin-typescript";
-
+import {merge} from 'lodash-es'
 const { resolve } = path;
 // https://vitejs.dev/config/
 export default ({ command, mode }) => {
@@ -101,6 +101,6 @@ export default ({ command, mode }) => {
     }
   };
 
-  _.merge(options, build);
+  merge(options, build);
   return options;
 };
