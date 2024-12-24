d217215fe7b79ed799e60a592895a6b1f8a6d6b0
xiaojunnuo <xiaojunnuo@qq.com>
2024-12-25 00:05:41 +0800

    chore: 提示优化

--- packages/ui/ui-interface/src/context.ts
+++ packages/ui/ui-interface/src/context.ts
@@ -11,7 +11,7 @@ export class UiContext {
   get(): UiInterface {
     if (this.ref.value == null) {
       throw new Error(
-        "您还未设置ui,先安装依赖@fast-crud/ui-interface,然后在use(FastCrud)前安装ui，app.use(UiXxx)，如果是NaiveUI，还需要用fs-ui-context包裹router-view，请参考http://fast-crud.docmirror.cn/guide/start/integration.html#_5-naiveui%E7%9A%84%E9%A2%9D%E5%A4%96%E6%93%8D%E4%BD%9C"
+        "您还未设置ui,第一步：先安装依赖@fast-crud/ui-interface,然后在use(FastCrud)前安装ui，app.use(UiXxx)；第二步：如果是NaiveUI，还需要用fs-ui-context包裹router-view，请参考http://fast-crud.docmirror.cn/guide/start/integration.html#_5-naiveui%E7%9A%84%E9%A2%9D%E5%A4%96%E6%93%8D%E4%BD%9C"
       );
     }
     return this.ref.value;
--- packages/ui/ui-naive/src/naive.tsx
+++ packages/ui/ui-naive/src/naive.tsx
@@ -206,7 +206,7 @@ export class Naive implements UiInterface {
     getInstance() {
       if (!this.instance) {
         throw new Error(
-          "您还未设置ui,先安装依赖@fast-crud/ui-interface,然后在use(FastCrud)前安装ui，app.use(UiXxx)，还需要用fs-ui-context包裹router-view，请参考http://fast-crud.docmirror.cn/guide/start/integration.html#_5-naiveui%E7%9A%84%E9%A2%9D%E5%A4%96%E6%93%8D%E4%BD%9C"
+          "您还未设置ui,第一步：先安装依赖@fast-crud/ui-interface,然后在use(FastCrud)前安装ui，app.use(UiXxx)；第二步：还需要用fs-ui-context包裹router-view，请参考http://fast-crud.docmirror.cn/guide/start/integration.html#_5-naiveui%E7%9A%84%E9%A2%9D%E5%A4%96%E6%93%8D%E4%BD%9C"
         );
       }
       if (this.instance instanceof Function) {
