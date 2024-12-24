189c8a906aa10a1f022e43e1549f5c5abe024aa6
xiaojunnuo <xiaojunnuo@qq.com>
2024-12-25 00:03:34 +0800

    chore: 提示优化

--- packages/ui/ui-interface/src/context.ts
+++ packages/ui/ui-interface/src/context.ts
@@ -10,7 +10,9 @@ export class UiContext {
 
   get(): UiInterface {
     if (this.ref.value == null) {
-      throw new Error("您还未设置ui,先安装依赖@fast-crud/ui-interface,然后在use(FastCrud)前安装ui，app.use(UiXxx)");
+      throw new Error(
+        "您还未设置ui,先安装依赖@fast-crud/ui-interface,然后在use(FastCrud)前安装ui，app.use(UiXxx)，如果是NaiveUI，还需要用fs-ui-context包裹router-view，请参考http://fast-crud.docmirror.cn/guide/start/integration.html#_5-naiveui%E7%9A%84%E9%A2%9D%E5%A4%96%E6%93%8D%E4%BD%9C"
+      );
     }
     return this.ref.value;
   }
--- packages/ui/ui-naive/src/naive.tsx
+++ packages/ui/ui-naive/src/naive.tsx
@@ -205,7 +205,9 @@ export class Naive implements UiInterface {
     name: "n-notification",
     getInstance() {
       if (!this.instance) {
-        throw new Error("请先配置ui");
+        throw new Error(
+          "您还未设置ui,先安装依赖@fast-crud/ui-interface,然后在use(FastCrud)前安装ui，app.use(UiXxx)，还需要用fs-ui-context包裹router-view，请参考http://fast-crud.docmirror.cn/guide/start/integration.html#_5-naiveui%E7%9A%84%E9%A2%9D%E5%A4%96%E6%93%8D%E4%BD%9C"
+        );
       }
       if (this.instance instanceof Function) {
         return this.instance();
