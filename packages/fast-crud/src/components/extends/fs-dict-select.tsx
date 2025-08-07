14965b8d21f522a6f9553cce0750a047339c861b
xiaojunnuo <xiaojunnuo@qq.com>
2025-08-07 15:37:01 +0800

    fix: 修复dict-select naive下不生效的bug

--- packages/fast-crud/src/components/extends/fs-dict-select.tsx
+++ packages/fast-crud/src/components/extends/fs-dict-select.tsx
@@ -117,6 +117,7 @@ export default defineComponent({
           placeholder={this.computedPlaceholder}
           options={options}
           renderLabel={this.renderLabel}
+          v-slots={this.slots}
           {...binding}
         />
       );
