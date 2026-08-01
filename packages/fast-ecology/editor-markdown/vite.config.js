6364e24ba2df19498ec01562c210e7872b442d09
xiaojunnuo <xiaojunnuo@qq.com>
2026-08-02 01:33:07 +0800

    chore: publish

--- packages/fast-ecology/editor-markdown/vite.config.js
+++ packages/fast-ecology/editor-markdown/vite.config.js
@@ -1,26 +1,107 @@
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import path from "path";
-
+import { visualizer } from "rollup-plugin-visualizer";
+import strip from "@rollup/plugin-strip";
+import DefineOptions from "unplugin-vue-define-options/vite";
+import typescript from "@rollup/plugin-typescript";
+import _ from "lodash";
 const { resolve } = path;
+// https://vitejs.dev/config/
+export default ({ command, mode }) => {
+  let build = {};
+  if (mode === "umd") {
+    build = {
+      build: {
+        emptyOutDir: true,
+        lib: {
+          entry: resolve(__dirname, "src/index.umd.ts"),
+          formats: ["umd"]
+        },
+        rollupOptions: {
+          output: {
+            manualChunks: null
+          }
+        }
+      }
+    };
+  }
 
-export default ({ mode }) => ({
-  plugins: [vue()],
-  build: {
-    emptyOutDir: mode === "umd",
-    lib: {
-      entry: resolve(__dirname, mode === "umd" ? "src/index.umd.ts" : "src/index.ts"),
-      name: "FsEditorMarkdown",
-      formats: [mode === "umd" ? "umd" : "es"]
+  const options = {
+    base: "./",
+    plugins: [DefineOptions(), vueJsx(), vue()],
+    esbuild: {
+      drop: command === "build" ? ["debugger"] : [],
+      jsxFactory: "h",
+      jsxFragment: "Fragment",
+      pure: ["console.log", "debugger"]
     },
-    rollupOptions: {
-      external: ["vue", "@fast-crud/fast-crud", "mavon-editor"],
-      output: {
-        globals: {
-          vue: "Vue",
-          "@fast-crud/fast-crud": "FastCrud",
-          "mavon-editor": "MavonEditor"
+    build: {
+      emptyOutDir: false,
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        name: "FsExtends",
+        formats: ["es"]
+      },
+      // cssCodeSplit: true,
+      sourcemap: true,
+      // minify: false,
+      rollupOptions: {
+        plugins: [
+          strip(),
+          visualizer(),
+          typescript({
+            target: "es2020",
+            rootDir: "src",
+            declaration: true,
+            declarationDir: "dist/d",
+            exclude: "./node_modules/**",
+            allowSyntheticDefaultImports: true
+          })
+        ],
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: [
+          "vue",
+          "lodash-es",
+          "dayjs",
+          "vue-i18n",
+          "vuedraggable",
+          "axios",
+          "qiniu-js",
+          "@fast-crud/fast-crud",
+          "@fast-crud/ui-interface",
+          "mavon-editor"
+        ],
+        output: {
+          manualChunks(id) {
+            if (id.includes("src/components") && id.lastIndexOf(".vue") > 0) {
+              let name = id.substring(id.lastIndexOf("/") + 1);
+              name = name.substring(0, name.indexOf("."));
+              return "components/" + name;
+            }
+          },
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            vue: "Vue",
+            "lodash-es": "_",
+            dayjs: "dayjs",
+            "vue-i18n": "VueI18n",
+            axios: "axios",
+            vuedraggable: "vuedraggable",
+            "@fast-crud/fast-crud": "FastCrud",
+            "ali-oss": "OSS",
+            "cos-js-sdk-v5": "COS",
+            "qiniu-js": "qiniu",
+            "@fast-crud/ui-interface": "FsUiInterface",
+            "mavon-editor": "MavonEditor"
+          }
         }
       }
     }
-  }
-});
+  };
+
+  _.merge(options, build);
+  return options;
+};
